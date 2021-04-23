7deea8831850e6eec9cf6c73e64271c1bf60c6b1
MudithcJay <mudith_blackmamba@hotmail.com>
2021-04-23 21:44:55 +0530

    Added random number endpoint / added flask cors

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+import random
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/random' , methods=["GET"])
+def getRandomNumbers():
+    num = []
+    for x in range(10):
+        num.append(random.randint(0,500))
+    return jsonify(num)
 
 
 if __name__ == '__main__':
