07085359382a4fd94f45e5782d091ee65d29d13e
MudithcJay <mudith_blackmamba@hotmail.com>
2021-04-23 23:47:53 +0530

    Basic Message Features added

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify , request
 from flask_cors import CORS
 import random
+import main as m
 
 app = Flask(__name__)
 CORS(app)
+
+main = m()
 
 @app.route('/')
 def hello_world():
@@ -17,6 +20,17 @@ def getRandomNumbers():
         num.append(random.randint(0,500))
     return jsonify(num)
 
+
+@app.route("/sendMsg" , methods=["POST"])
+def sendMessage():
+    data = request.get_data().decode('utf-8')
+    data = data.split(",")
+    main.addMessage(data[0] , data[1])
+    return jsonify(main.getMessages())
+
+@app.route("/getMessages" , methods=["GET"])
+def getMessages():
+    return jsonify(main.getMessages())
 
 if __name__ == '__main__':
     app.run()
---
+++ main.py
@@ -0,0 +1,15 @@
+
+class main:
+
+    def __init__(self):
+        self.messages = []
+        self.msgUsers = []
+
+    def addMessage(self , message , userName):
+        self.messages.append(message)
+        self.msgUsers.append(userName)
+
+
+    def getMessages(self):
+        return [self.messages,self.msgUsers]
+
